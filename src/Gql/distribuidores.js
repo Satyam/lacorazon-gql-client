a87adb821291562b8ae6f3d00846d637f6004bbe
Satyam <satyam@satyam.com.ar>
2019-06-02 19:47:01 +0200

    Added mutations to resolvers

--- src/Gql/distribuidores.js
+++ src/Gql/distribuidores.js
@@ -30,6 +30,69 @@ query ($id: ID!) {
 }
 `;
 
-export const CREATE_DISTRIBUIDOR = ``;
-export const UPDATE_DISTRIBUIDOR = ``;
-export const DELETE_DISTRIBUIDOR = ``;
+export const CREATE_DISTRIBUIDOR = `mutation (
+  $nombre: String!
+  $email: String
+  $localidad: String
+  $contacto: String
+  $telefono: String
+  $direccion: String
+) {
+  createDistribuidor(
+    nombre: $nombre, 
+    email: $email, 
+    localidad: $localidad, 
+    contacto: $contacto,
+    telefono: $telefono,
+    direccion: $direccion
+  ) {
+    id
+    nombre
+    localidad
+    contacto
+    telefono
+    email
+    direccion
+  }
+}
+`;
+
+export const UPDATE_DISTRIBUIDOR = `mutation (
+  $id: ID!,
+  $nombre: String
+  $email: String
+  $localidad: String
+  $contacto: String
+  $telefono: String
+  $direccion: String
+) {
+  updateDistribuidor(
+    id: $id, 
+    nombre: $nombre, 
+    email: $email, 
+    localidad: $localidad, 
+    contacto: $contacto,
+    telefono: $telefono,
+    direccion: $direccion
+   ) {
+    id
+    nombre
+    localidad
+    contacto
+    telefono
+    email
+    direccion
+  }
+}`;
+
+export const DELETE_DISTRIBUIDOR = `mutation ($id: ID!) {
+  deleteDistribuidor(id: $id) {
+    id
+    nombre
+    localidad
+    contacto
+    telefono
+    email
+    direccion
+  }
+}`;
