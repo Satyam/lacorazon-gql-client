c3243d831d11896d76eedf031136a577cfa0510e
Satyam <satyam@satyam.com.ar>
2019-07-20 19:36:37 +0200

    Moved `Page` component to a folder of its own so it can have its  stylesheet

--- src/Components/Page/index.js
+++ src/Components/Page/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 
+import styles from './styles.module.css';
+
 export default function Page({ wide, children, title, heading, action }) {
   document.title = `La Corazón - ${title}`;
   return (
     <Container fluid>
       <Row>
         <Col sm="12" md={{ size: wide ? 12 : 8, offset: wide ? 0 : 2 }}>
-          <h1 style={{ float: 'left' }}>{heading}</h1>
-          <span style={{ float: 'right' }}>{action}</span>
+          <div className={styles.spacing}>
+            <h1 className={styles.heading}>{heading}</h1>
+            <span className={styles.action}>{action}</span>
+          </div>
           {children}
         </Col>
       </Row>
