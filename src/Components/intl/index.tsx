56f1621fc0026d50a12803d4e3740877981a7d58
Satyam <satyam@satyam.com.ar>
2019-07-28 17:37:05 +0200

    typescripted `useIntl`

--- src/Components/intl/index.tsx
+++ src/Components/intl/index.tsx
@@ -11,20 +11,39 @@ import { enUS, es } from 'date-fns/locale';
 
 import { registerLocale, setDefaultLocale } from 'react-datepicker';
 
-const localeTables = { 'en-US': enUS, 'es-ES': es };
+const localeTables: { [index: string]: any } = { 'en-US': enUS, 'es-ES': es };
 
 Object.keys(localeTables).forEach(l => registerLocale(l, localeTables[l]));
 
-export const IntlContext = createContext({});
+type intlType = {
+  locales: string[];
+  setLocale: (locale: string) => void;
+  locale: string;
+  formatDate: (date: object) => string;
+  currency: string;
+  setCurrency: (currency: string) => void;
+  formatCurrency: (amount: number) => string;
+};
 
-export function IntlProvider({
-  locale: l = navigator.locale,
-  currency: c = 'EUR',
-  children,
-}) {
+export const IntlContext = createContext<intlType>({
+  locales: Object.keys(localeTables),
+  setLocale: l => undefined,
+  locale: navigator.language,
+  formatDate: d => String(d),
+  currency: 'EUR',
+  setCurrency: c => undefined,
+  formatCurrency: c => String(c),
+});
+
+export const IntlProvider: React.FC<{
+  locale: string;
+  currency: string;
+}> = ({ locale: l = navigator.language, currency: c = 'EUR', children }) => {
   const [locale, setLocale] = useState(l);
   const [currency, setCurrency] = useState(c);
-  const [formatCurrency, setFormatCurrency] = useState(value => value);
+  const [formatCurrency, setFormatCurrency] = useState<
+    (value: number) => string
+  >(value => String(value));
 
   const formatDate = useCallback(
     (date, formatStr = 'P') =>
@@ -43,7 +62,7 @@ export function IntlProvider({
       style: 'currency',
       currency,
     });
-    setFormatCurrency(() => value => currFormatter.format(value));
+    setFormatCurrency(() => (value: number) => currFormatter.format(value));
   }, [currency, locale]);
 
   return (
@@ -61,7 +80,7 @@ export function IntlProvider({
       {children}
     </IntlContext.Provider>
   );
-}
+};
 
 export function useIntl() {
   return useContext(IntlContext);
