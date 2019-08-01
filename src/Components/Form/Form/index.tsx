325ca8b8114dbddd3700a2280a6715e188cc9743
Satyam <satyam@satyam.com.ar>
2019-08-01 19:19:38 +0200

    Improved my `Form` component by: * passing `formikBag` to `onSubmit` as the original does * dropping the use of Formik's `Form` component, instead adding the needed Formik handlers to a regular reactstrap Form

--- src/Components/Form/Form/index.tsx
+++ src/Components/Form/Form/index.tsx
@@ -1,30 +1,33 @@
 import React from 'react';
 import { Form as BSForm, Alert } from 'reactstrap';
-import { Formik, Form as KForm, FormikValues } from 'formik';
+import { Formik, FormikValues, FormikHelpers } from 'formik';
 import { Schema } from 'yup';
 
 const Form: React.FC<{
   schema?: Schema<any>;
   values?: FormikValues;
-  onSubmit: (values: any) => any | Promise<any>;
+  onSubmit: (
+    values: any,
+    formikBag: FormikHelpers<FormikValues>
+  ) => any | Promise<any>;
   formProps?: any;
 }> = ({ schema, values, onSubmit, children, formProps, ...rest }) => (
   <Formik
     validationSchema={schema}
     initialValues={schema ? Object.assign(schema.default(), values) : values}
-    onSubmit={(values, { setStatus }) => {
-      const result = onSubmit(schema ? schema.cast(values) : values);
+    onSubmit={(values, formikBag) => {
+      const result = onSubmit(schema ? schema.cast(values) : values, formikBag);
       if (result && typeof result.then === 'function') {
         return result.catch((err: any) => {
-          setStatus(err);
+          formikBag.setStatus(err);
         });
       }
       return result;
     }}
     {...rest}
   >
-    {({ status }) => (
-      <BSForm tag={KForm} {...formProps}>
+    {({ status, handleReset, handleSubmit }) => (
+      <BSForm onSubmit={handleSubmit} onReset={handleReset} {...formProps}>
         {status && <Alert color="danger">{status}</Alert>}
         {children}
       </BSForm>
