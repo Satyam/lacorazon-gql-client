4ffa3e30cba8724ea288ae02fa9573d977c2d252
Satyam <satyam@satyam.com.ar>
2019-08-07 16:13:37 +0200

    Moved Apollo provider initialization to separate component

---
+++ src/Components/Apollo/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: createHttpLink({
+    uri: '/graphql',
+    credentials: 'same-origin',
+  }),
+});
+
+export const GqlProvider: React.FC<{}> = ({ children }) => (
+  <ApolloProvider client={client}>{children}</ApolloProvider>
+);
+
+export default GqlProvider;
