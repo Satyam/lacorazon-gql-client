6f0e1e6338ccf48dec4c3550417d140076b51290
Satyam <satyam@satyam.com.ar>
2019-08-28 16:43:11 +0200

    Simplified `GqlProvider` by switching to `useReducer` and using static functions.

--- src/Providers/Apollo/index.tsx
+++ src/Providers/Apollo/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useMemo } from 'react';
+import React, { useEffect, useReducer } from 'react';
 
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from '@apollo/react-hooks';
@@ -8,55 +8,78 @@ import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 
 import { useAuth0 } from 'Providers/Auth';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+  credentials: 'same-origin',
+});
+
+const unauthorizedClient: ApolloClient<
+  NormalizedCacheObject
+> = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: httpLink,
+});
+
+type ActionType =
+  | {
+      type: 'withUser';
+      token: string;
+    }
+  | {
+      type: 'noUser';
+    };
+
+export const reducer = (
+  state: ApolloClient<NormalizedCacheObject>,
+  action: ActionType
+): ApolloClient<NormalizedCacheObject> => {
+  console.log(action);
+
+  switch (action.type) {
+    case 'withUser': {
+      const authLink = setContext((_, { headers }) => ({
+        headers: {
+          ...headers,
+          authorization: `Bearer ${action.token}`,
+        },
+      }));
+      return new ApolloClient({
+        cache: new InMemoryCache(),
+        link: authLink.concat(httpLink),
+      });
+    }
+    case 'noUser':
+      return unauthorizedClient;
+    default:
+      return state;
+  }
+};
 export const GqlProvider: React.FC<{}> & { whyDidYouRender?: boolean } = ({
   children,
 }) => {
   const { loading, getTokenSilently, isAuthenticated } = useAuth0();
-  const httpLink = useMemo(
-    () =>
-      createHttpLink({
-        uri: '/graphql',
-        credentials: 'same-origin',
-      }),
-    []
-  );
 
-  const plainLink: ApolloClient<NormalizedCacheObject> = useMemo(
-    () =>
-      new ApolloClient({
-        cache: new InMemoryCache(),
-        link: httpLink,
-      }),
-    [httpLink]
-  );
-
-  const clientRef = useRef(plainLink);
+  const [client, dispatch] = useReducer(reducer, unauthorizedClient);
 
   useEffect(() => {
-    const newLink = () => {
-      if (loading) return;
-      if (isAuthenticated) {
-        getTokenSilently().then(token => {
-          const authLink = setContext((_, { headers }) => ({
-            headers: {
-              ...headers,
-              authorization: token ? `Bearer ${token}` : '',
-            },
-          }));
-          clientRef.current = new ApolloClient({
-            cache: new InMemoryCache(),
-            link: authLink.concat(httpLink),
+    if (loading) return;
+    if (isAuthenticated) {
+      getTokenSilently().then(token => {
+        if (token) {
+          dispatch({
+            type: 'withUser',
+            token,
           });
-        });
-      } else {
-        clientRef.current = plainLink;
-      }
-    };
-    newLink();
-    return;
-  }, [loading, getTokenSilently, isAuthenticated, httpLink, plainLink]);
+        } else {
+          dispatch({ type: 'noUser' });
+        }
+      });
+    } else {
+      dispatch({ type: 'noUser' });
+    }
+  }, [loading, getTokenSilently, isAuthenticated]);
 
-  return <ApolloProvider client={clientRef.current}>{children}</ApolloProvider>;
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
 };
 
 GqlProvider.whyDidYouRender = true;
