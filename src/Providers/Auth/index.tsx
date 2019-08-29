e6cd7fe11421c0371cd0dd9c624bdf7b6d000ec7
Satyam <satyam@satyam.com.ar>
2019-08-29 17:56:23 +0200

    Added the  Auth0 rule source as a comment within the provider code since they are so closely related.

--- src/Providers/Auth/index.tsx
+++ src/Providers/Auth/index.tsx
@@ -171,3 +171,33 @@ export const Auth0Provider: React.FC<Auth0ClientOptions> = ({
 
   return <Auth0Context.Provider value={ctx}>{children}</Auth0Context.Provider>;
 };
+
+/*
+// This Rule is expected to be active in the Auth0 dashboard rules for this app
+
+// NAMESPACE is expected to be the same as the `audience` argument passed to the provider
+
+function (user, context, callback) {
+  var ManagementClient = require('auth0@2.18.1').ManagementClient;
+  var management = new ManagementClient({
+    token: auth0.accessToken,
+    domain: auth0.domain
+  });
+  var params = { id: user.user_id, page: 0, per_page: 50, include_totals: true };
+  management.getUserPermissions(params, (err, permissions) => {
+    if (err) {
+      // Handle error.
+      console.log('err: ', err);
+      callback(err);
+    } else {
+      context.idToken[configuration.NAMESPACE + 'user_authorization'] = {
+        permissions: permissions.permissions.map(
+          permission => 	permission.permission_name
+        )
+      };
+ 			callback(null, user, context);
+    }
+  });
+}
+
+*/
