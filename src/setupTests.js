628ec7a23aecd6bb9cbbdbfb990fec03075c7e28
Satyam <satyam@satyam.com.ar>
2020-06-24 17:28:59 +0200

    Fixed as per: https://react-hook-form.com/faqs/#TestingfailedduetoMutationObserver https://github.com/testing-library/react-testing-library/issues/141#issuecomment-406338416

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,8 @@
 import '@testing-library/jest-dom/extend-expect';
+import 'mutationobserver-shim';
 
 const snapshotDiff = require('snapshot-diff');
 
 expect.addSnapshotSerializer(snapshotDiff.getSnapshotDiffSerializer());
+
+global.MutationObserver = global.window.MutationObserver;
